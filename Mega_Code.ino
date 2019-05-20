c1b25c1cc07fac5646b4fa504b87f7eefedd7f41
srikanth murikipudi <50315236+srikanth-murikipudi0506@users.noreply.github.com>
2019-05-20 16:26:38 +0530

    Add files via upload

---
+++ Mega_Code.ino
@@ -0,0 +1,29 @@
+
+
+void setup() {
+  
+  //Serial1.begin(9600);//Using Serial1 Port
+  Serial.begin(9600);
+  pinMode(8, OUTPUT);//DE/RE Controling pin of RS-485
+}
+
+void loop() {
+  char getdata='m';
+
+  digitalWrite(8,HIGH);//DE/RE=HIGH Transmit Enabled M1
+  Serial.print('9');//Write '9' and Fetch Data From Pro Mini
+   
+  digitalWrite(8,LOW);//DE/RE=LOW Receive Enabled M1
+  delay(1000);
+  
+  if(Serial.available()){ //If Serial Data is available
+    
+    while(Serial.available() && getdata!='d')
+    { 
+    getdata=Serial.read();
+    Serial.print(getdata);
+    }
+    
+    Serial.println("");
+    }
+}
